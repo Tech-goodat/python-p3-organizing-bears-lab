c2694a7be8f42295bf2e7d902026b1e37daa1f1a
Tech-goodat <felixkiprotich2000@gmail.com>
2023-12-05 17:20:06 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 3, 'M', 'Grey', 'Humble', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'F', 'Grey', 'Humble', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'M', 'Grey', 'Agressive', 'no');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'F', 'Grey', 'Humble', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'M', 'Black', 'Humble', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'F', 'Black', 'Agressive', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'M', 'Black', 'Humble', 'yes');
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'M', 'Black', 'Humble', 'yes');
+
